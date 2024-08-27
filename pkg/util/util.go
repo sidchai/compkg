6e32c8622cd2bddd9e01addcac908d273d318e41
sidchai <sidchai@163.com>
2024-08-27 18:16:53 +0800

    feat(varied): 工具类新增获取ip方法

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 	"math"
 	"math/rand"
+	"net"
 	"os"
 	"reflect"
 	"strings"
@@ -137,4 +138,15 @@ func CheckFolder(path string) bool {
 		return false
 	}
 	return false
+}
+
+func GetOurBoundIP() (ip string, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	ip = strings.Split(localAddr.IP.String(), ":")[0]
+	return
 }
